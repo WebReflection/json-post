8b33ab6cae40fa9b9262f72a94ec7a4e25caa9fd
Andrea Giammarchi <a.giammarchi@eyeo.com>
2018-08-28 01:00:04 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # json-post
-How to post JSON in NodeJS
+How to post JSON in NodeJS.
+
+<sup><sub>You know those simplest things ever not properly documented online? That!</sub></sup>
+
+```js
+const postJSON = require('post-json');
+// or import postJSON from 'post-json'
+
+postJSON(
+  'https://whatever:5000/seriously',
+  {hello: 'world'}
+).then(
+  console.info,
+  console.error
+);
+```
+
+That's literally it.
---
+++ index.js
@@ -0,0 +1,41 @@
+/*!
+ * ISC License
+ *
+ * Copyright (c) 2018, Andrea Giammarchi, @WebReflection
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+ * OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
+const postJSON = (where, data) => new Promise((resolve, reject) => {
+  const json = JSON.stringify(data);
+  const url = require('url').parse(where);
+  const request = require(url.protocol.slice(0, -1)).request(
+    {
+      hostname: url.hostname,
+      port: url.port,
+      path: url.path,
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'content-length': json.length
+      }
+    },
+    res => {
+      res.setEncoding('utf8');
+      const data = [];
+      res.on('data', chunk => data.push(chunk));
+      res.on('end', () => resolve(data.join('')));
+    }
+  );
+  request.on('error', reject);
+  request.end(json);
+});
